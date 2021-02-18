88aff00f07452c0709f5bc5031069ccc6aa11ddb
nigavshin <n.a.gavshin@kalashnikovconcern.ru>
2021-02-18 17:14:27 +0400

    feat:add styles header

--- src/index.js
+++ src/index.js
@@ -6,5 +6,5 @@ import logo from './images/logo.png'
 let container = document.createElement('div');
 container.className = "container";
 document.body.append(container);
-container.insertAdjacentHTML('beforeend','<header class="header"><img class="logo" src="./images/logo.png" alt="logo"><form><div class="search"><input type="search" name="q"><input type="submit" value=""></div></form><button class="dtn-entry">Войти</button></header>');
+container.insertAdjacentHTML('beforeend','<header class="header"><img class="logo" src="./images/logo.png" alt="logo"><form class="input-wrapper" data-text=""><input type="text" placeholder="Поиск…"><button class="search-btn">Найти</button></form><button class="btn-entry">Войти</button></header>');
 
